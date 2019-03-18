3446d3f958e45e1c02b169b194efb5476ac423ec
zhangyi <zy84338719@hotmail.com>
2019-03-18 09:47:51 +0800

    更新init

--- init.sh
+++ init.sh
@@ -9,8 +9,7 @@ echo "alias ips='~/.tools/ips'" >> ~/.zshrc
 cat ./rm_alias >> ~/.zshrc
 source ~/.zshrc
 
-echo "添加环境变量到~/.fish"
-
-echo "alias ips='~/.tools/ips'" >> ~/.config/fish/config.fish
-echo "alias up='~/.tools/up'" >> ~/.config/fish/config.fish
-cat ./rm_alias >>  ~/.config/fish/config.fish
+#echo "添加环境变量到~/.fish"
+#echo "alias ips='~/.tools/ips'" >> ~/.config/fish/config.fish
+#echo "alias up='~/.tools/up'" >> ~/.config/fish/config.fish
+#cat ./rm_alias >>  ~/.config/fish/config.fish
