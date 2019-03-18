351ea5f751c97b7b560bd30c81826343f71f96d4
zhangyi <zy84338719@hotmail.com>
2019-03-18 09:17:58 +0800

    init 添加初始化方法

--- init.sh
+++ init.sh
@@ -2,6 +2,15 @@
 mkdir ~/.tools
 cp ./up ~/.tools
 cp ./ips ~/.tools
+
+echo "添加环境变量到~/.zshrc" 
 echo "alias up='~/.tools/up'" >> ~/.zshrc
 echo "alias ips='~/.tools/ips'" >> ~/.zshrc
-echo ./rm_alias >> ~/.zsh
+cat ./rm_alias >> ~/.zshrc
+source ~/.zshrc
+
+echo "添加环境变量到~/.fish"
+
+echo "alias ips='~/.tools/ips'" >> ~/.config/fish/config.fish
+echo "alias up='~/.tools/up'" >> ~/.config/fish/config.fish
+cat ./rm_alias >>  ~/.config/fish/config.fish
